5ace9d4fdc7dc15df954edc850f17904d768e58a
Debabrata Manna <nicdebabratamanna@gmail.com>
2019-11-27 20:10:49 +0530

    First commit

---
+++ src/app/shared/order.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Order } from './order.model';
+import { OrderItem } from './order-item.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+  
+  formData:Order;
+  orderItem:OrderItem[];
+  constructor() { }
+}
